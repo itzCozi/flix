5cb8663df59b8ad1b2b761724205a5a12a09a15d
Cooper <Cooperransom08@gmail.com>
2024-07-23 00:48:45 +0000

    Update template

--- functions/template.ts
+++ functions/template.ts
@@ -7,14 +7,14 @@ export function getTemplate({
 }): string {
   return `
   <!doctype html>
-  <html lang="en" data-theme="dark">
+    <html lang="en" data-theme="light">
 
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Password Entry</title>
-      <meta name="description" content="This is password protected.">
-      <link rel="shortcut icon" href="https://picocss.com/favicon.ico">
+      <title>Authentication</title>
+      <meta name="description" content="This is password protected. (sorry :3)">
+      <link rel="icon" href="https://i.postimg.cc/RZ9dhRyr/locked-padlock.png">
 
       <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
 
@@ -27,14 +27,19 @@ export function getTemplate({
           min-height: 100vh;
           padding: 1rem 0;
           margin: 0 auto;
-          max-width: 600px;
+          max-width: 55em;
+        }
+        
+        h1 + h2 {
+          margin-top: 0.5rem;
         }
 
         .error {
-          background: white;
-          border-radius: 10px;
-          color: var(--del-color);
-          padding: 0.5em 1em;
+          background: #5f9ae8;
+          border-radius: 0.35em;
+          color: #fefefe;
+          padding: 0.5em;
+          box-shadow: 0 0.25em 0.5em rgba(0, 0, 0, 0.2);
         }
 
         h2 { color: var(--color-h2); }
@@ -45,14 +50,14 @@ export function getTemplate({
       <main>
         <article>
           <hgroup>
-            <h1>Password</h1>
-            <h2>Enter the password for this site.</h2>
+            <h1>Sight Guard</h1>
+            <h2>Enter the correct string to continue...</h2 >
           </hgroup>
-          ${withError ? `<p class="error">Incorrect password, please try again.</p>` : ''}
+          ${withError ? `<p class="error">Incorrect entry, access denied.</p>` : ''}
           <form method="post" action="/cfp_login">
             <input type="hidden" name="redirect" value="${redirectPath}" />
-            <input type="password" name="password" placeholder="Password" aria-label="Password" autocomplete="current-password" required autofocus>
-            <button type="submit" class="contrast">Login</button>
+            <input type="password" name="password" placeholder="********" aria-label="Password" autocomplete="current-password" required autofocus>
+            <button type="submit" class="contrast">Continue</button>
           </form>
         </article>
       </main>
